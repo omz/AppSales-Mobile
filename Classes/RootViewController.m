1c2a776cf3654f7dd307072b7231887fd752098b
Evan Schoenberg <evands@Shindig.local>
2010-04-05 08:50:57 -0500

    On iPad, the status label should be dark to appear properly over a toolbar.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -45,6 +45,7 @@
 #import "ReportManager.h"
 #import "ReviewsController.h"
 #import "ImportExportViewController.h"
+#import "UIDevice+iPad.h"
 
 @implementation RootViewController
 
@@ -65,8 +66,13 @@
 	UIBarButtonItem *flexSpaceItem = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil] autorelease];
 	
 	self.statusLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 150, 32)] autorelease];
-	statusLabel.textColor = [UIColor whiteColor];
+	
+	if ([UIDevice currentDevice].isPad)
+		statusLabel.textColor = [UIColor darkGrayColor];
+	else
+		statusLabel.textColor = [UIColor whiteColor];
 	statusLabel.shadowColor = [UIColor darkGrayColor];
+
 	statusLabel.shadowOffset = CGSizeMake(0, 1);
 	statusLabel.font = [UIFont systemFontOfSize:12.0];
 	statusLabel.numberOfLines = 2;
