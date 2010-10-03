d820cbe7fffddad82aa67cc32bad927f56385fbb
Evan <evan@evans-macbook-3.local>
2010-10-04 02:23:36 +0800

    Go through vendor default page to allow downloading of reports

--- Classes/ReportManager.m
+++ Classes/ReportManager.m
@@ -39,7 +39,7 @@
 	if (self) {
 		days = [NSMutableDictionary new];
 		weeks = [NSMutableDictionary new];
-
+		
 		BOOL cacheLoaded = [self loadReportCache];
 		if (!cacheLoaded) {
 			[[ProgressHUD sharedHUD] setText:NSLocalizedString(@"Updating Cache...",nil)];
@@ -167,7 +167,7 @@
 	}
 	
 	isRefreshing = YES;
-
+	
 	NSArray *daysToSkip = [days allKeys];
 	NSArray *weeksToSkip = [weeks allKeys];
 	NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:
@@ -179,7 +179,7 @@
                               nil];
 	[self performSelectorInBackground:@selector(fetchReportsWithUserInfo:) withObject:userInfo];
 }
-
+#define ITTS_VENDOR_DEFAULT_URL @"https://reportingitc.apple.com/vendor_default.faces"
 #define ITTS_SALES_PAGE_URL @"https://reportingitc.apple.com/sales.faces"
 
 static NSMutableArray* extractFormOptions(NSString *htmlPage, NSString *formID) {
@@ -277,6 +277,7 @@ static Day* downloadReport(NSString *originalReportsPath, NSString *ajaxName, NS
 - (void)fetchReportsWithUserInfo:(NSDictionary *)userInfo
 {
 	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	NSScanner *scanner;
     [self performSelectorOnMainThread:@selector(setProgress:) withObject:NSLocalizedString(@"Starting Download...",nil) waitUntilDone:NO];
     
 	NSArray *daysToSkipDates = [userInfo objectForKey:@"daysToSkip"];
@@ -309,7 +310,7 @@ static Day* downloadReport(NSString *originalReportsPath, NSString *ajaxName, NS
         [self performSelectorOnMainThread:@selector(setProgress:) withObject:NSLocalizedString(@"Logging in...",nil) waitUntilDone:NO];
         
         // find the login action
-        NSScanner *scanner = [NSScanner scannerWithString:loginPage];
+        scanner = [NSScanner scannerWithString:loginPage];
         [scanner scanUpToString:@"action=\"" intoString:nil];
         if (! [scanner scanString:@"action=\"" intoString:nil]) {
             [self performSelectorOnMainThread:@selector(downloadFailed:) withObject:@"could not parse iTunes Connect login page" waitUntilDone:NO];
@@ -350,7 +351,28 @@ static Day* downloadReport(NSString *originalReportsPath, NSString *ajaxName, NS
         [pool release];
         return;
     }
-    	
+	
+	scanner = [NSScanner scannerWithString:salesRedirectPage];
+	NSString *viewState = parseViewState(salesRedirectPage);
+	//[scanner scanUpToString:@"script id=\"defaultVendorPage:" intoString:nil];
+	[scanner scanUpToString:@"script id=\"defaultVendorPage:" intoString:nil];
+	if (! [scanner scanString:@"script id=\"defaultVendorPage:" intoString:nil]) {
+		[self performSelectorOnMainThread:@selector(downloadFailed:) withObject:@"could not parse sales redirect page" waitUntilDone:NO];
+		[pool release];
+		return;
+	}
+	NSString *defaultVendorPage = nil;
+	[scanner scanUpToString:@"\"" intoString:&defaultVendorPage];
+	
+	// click though from the dashboard to the sales page
+    NSDictionary *reportPostData = [NSDictionary dictionaryWithObjectsAndKeys:
+									[defaultVendorPage stringByReplacingOccurrencesOfString:@"_2" withString:@"_0"], @"AJAXREQUEST",
+									viewState, @"javax.faces.ViewState",
+									defaultVendorPage, @"defaultVendorPage",
+									[@"defaultVendorPage:" stringByAppendingString:defaultVendorPage],[@"defaultVendorPage:" stringByAppendingString:defaultVendorPage],
+									nil];
+    NSString *reportResponse = getPostRequestAsString(ITTS_VENDOR_DEFAULT_URL, reportPostData);
+	
     // get the form field names needed to download the report
     NSString *salesPage = [NSString stringWithContentsOfURL:[NSURL URLWithString:ITTS_SALES_PAGE_URL] usedEncoding:NULL error:NULL];
     if (salesPage.length == 0) {
@@ -359,13 +381,14 @@ static Day* downloadReport(NSString *originalReportsPath, NSString *ajaxName, NS
         [pool release];
         return;
     }
+	
     
-    NSString *viewState = parseViewState(salesPage);    
-    NSString *dailyName = [salesPage stringByMatching:@"theForm:j_id_jsp_[0-9]*_21"];
-    NSString *weeklyName = [dailyName stringByReplacingOccurrencesOfString:@"_21" withString:@"_22"];
-    NSString *ajaxName = [dailyName stringByReplacingOccurrencesOfString:@"_21" withString:@"_2"];
-    NSString *daySelectName = [dailyName stringByReplacingOccurrencesOfString:@"_21" withString:@"_30"];
-    NSString *weekSelectName = [dailyName stringByReplacingOccurrencesOfString:@"_21" withString:@"_35"];
+    viewState = parseViewState(salesPage);    
+    NSString *dailyName = [salesPage stringByMatching:@"theForm:j_id_jsp_[0-9]*_6"];
+    NSString *weeklyName = [dailyName stringByReplacingOccurrencesOfString:@"_6" withString:@"_22"];
+    NSString *ajaxName = [dailyName stringByReplacingOccurrencesOfString:@"_6" withString:@"_2"];
+    NSString *daySelectName = [dailyName stringByReplacingOccurrencesOfString:@"_6" withString:@"_32"];
+    NSString *weekSelectName = [dailyName stringByReplacingOccurrencesOfString:@"_6" withString:@"_35"];
     
     // parse days available
     NSMutableArray *availableDays = extractFormOptions(salesPage, @"theForm:datePickerSourceSelectElement");
@@ -390,7 +413,7 @@ static Day* downloadReport(NSString *originalReportsPath, NSString *ajaxName, NS
     NSString *arbitraryWeek = [availableWeeks objectAtIndex:0];
     [availableWeeks removeObjectsInArray:weeksToSkip];
     [availableWeeks sortUsingSelector:@selector(compare:)];
-
+	
     
     // click though from the dashboard to the sales page
     NSDictionary *postDict = [NSDictionary dictionaryWithObjectsAndKeys:
@@ -453,7 +476,7 @@ static Day* downloadReport(NSString *originalReportsPath, NSString *ajaxName, NS
             return;
         }
     }
-
+	
 	if (availableDays.count == 0 && availableWeeks.count == 0) {
 		[self performSelectorOnMainThread:@selector(setProgress:) withObject:NSLocalizedString(@"No new reports found",nil) waitUntilDone:NO];
 	} else {
@@ -480,7 +503,7 @@ static Day* downloadReport(NSString *originalReportsPath, NSString *ajaxName, NS
     NSAssert([NSThread isMainThread], nil);
 	[UIApplication sharedApplication].idleTimerDisabled = NO;
 	NSString *message = NSLocalizedString(
-@"Sorry, an error occured when trying to download the report files. Please check your username, password and internet connection.",nil);
+										  @"Sorry, an error occured when trying to download the report files. Please check your username, password and internet connection.",nil);
 	if (error) {
 		message = [message stringByAppendingFormat:@"\n%@", error];
 	}
