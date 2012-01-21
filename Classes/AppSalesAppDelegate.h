adbd6ed72cc87f6b5b546b3c4b42b6bfd6859e79
Adar Porat <adar.porat@gmail.com>
2012-01-21 15:10:59 -0500

    passcode lock support
    using KKPasscodeLock

--- Classes/AppSalesAppDelegate.h
+++ Classes/AppSalesAppDelegate.h
@@ -8,10 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "AccountsViewController.h"
+#import "KKPasscodeLock.h"
 
 @class ASAccount;
 
-@interface AppSalesAppDelegate : NSObject <UIApplicationDelegate, UIActionSheetDelegate, AccountsViewControllerDelegate>
+@interface AppSalesAppDelegate : NSObject <UIApplicationDelegate, UIActionSheetDelegate, AccountsViewControllerDelegate, KKPasscodeViewControllerDelegate>
 {
 	UIWindow *window;
 	
